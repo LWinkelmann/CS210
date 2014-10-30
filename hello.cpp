2c2f07a4d914a1da3357aac7057e84d377cad2df
LWinkelmann <lwinkelmann94@gmail.com>
2014-10-30 09:17:15 -0700

    Added a default value

--- hello.cpp
+++ hello.cpp
@@ -1,8 +1,26 @@
+// Author: Lucas Winkelmann
+// Class: CS210
+// Descrpition: Prints hello and username
+
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main(int argc, char* argv[])
 {
-	cout << "Hello " << argv[10]  << endl;
+	// Initilize the string name
+	string name;
+	// Make the name equal to the username that is loged in via ssh
+	name = argv[10];
+	// If name does not exist then simply print hello world.
+	if (name == " ")	
+		cout << "Hello World" << endl;
+	//Otherwise print out Hello, username
+	else
+	{
+		name.erase(0, 5);
+		cout << "Hello " << name  << endl;
+	}
 }
+
